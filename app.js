af946f3754eb1b2e4f62a595f4e00eaa652a984e
dzulfilove <129120910+dzulfilove@users.noreply.github.com>
2024-07-30 14:09:14 +0700

    dhsjd

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
+const axios = require("axios");
+const cron = require("node-cron");
 
 const KemilingRoute = require("./src/Routes/KemilingRoute");
+const GtsKemilingRoute = require("./src/Routes/GtsKemilingRoute");
+const GtsTirtaRoute = require("./src/Routes/GtsTirtayasaRoute");
 const GadingRoute = require("./src/Routes/GadingRoute");
 const BugisRoute = require("./src/Routes/BugisRoute");
 const RajabasaRoute = require("./src/Routes/RajabasaRoute");
+const RunRoute = require("./src/Routes/RunAllRoute");
 const UripRoute = require("./src/Routes/UripRoute");
 const TuguRoute = require("./src/Routes/TuguRoute");
 const TirtayasaRoute = require("./src/Routes/TirtayasaRoute");
 const PanjangRoute = require("./src/Routes/PanjangRoute");
 const TelukRoute = require("./src/Routes/TelukRoute");
 const PalapaRoute = require("./src/Routes/PalapaRoute");
-const RunRoute = require("./src/Routes/RunAllRoute");
-const GtsTirtaRoute = require("./src/Routes/GtsTirtayasaRoute");
-const GtsKemilingRoute = require("./src/Routes/GtsKemilingRoute");
 
 const port = 5005;
-const cors = require("cors");
 const app = express();
 
 app.use(cors());
-// app.use(cors());
 app.use(express.json());
 
 // Menambahkan handler untuk root path
@@ -42,6 +42,20 @@ app.use(TirtayasaRoute);
 app.use(PanjangRoute);
 app.use(TelukRoute);
 app.use(PalapaRoute);
+
+// Menjadwalkan cron job
+const schedule = "0 8,13,17,15 14 * * *"; // At 8 AM, 1 PM, and 5 PM every day
+
+cron.schedule(schedule, () => {
+  axios
+    .get("http://202.157.189.177:5005/run")
+    .then((response) => {
+      console.log(`Success: ${response.data}`);
+    })
+    .catch((error) => {
+      console.error(`Error: ${error.message}`);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Server berjalan di port: ${port}`);
